5c023843db7a6cb06f9dee0273e5d12fe1c8c34d
Marcelo Pereira <mmp@br.ibm.com>
2015-12-28 11:37:35 -0200

    first commit

--- un_analysis.R
+++ un_analysis.R
@@ -1,3 +1,6 @@
+library(reshape2)
+library(dplyr)
+
 # Load the train and test data
 X_train <- read.table("X_train.txt")
 X_test <- read.table("X_test.txt")
@@ -11,13 +14,19 @@ colnames(X_test) <- features[,2]
 X_train <- cbind(TypeOfData = "training",X_train)
 X_test <- cbind(TypeOfData = "test",X_test)
 
-# Merge the training dataset with the activity number 
+# Merge the training dataset with the activity and subject number 
 Y_train <- read.table("Y_train.txt")
-trainingDataSet <- cbind(Y_train,X_train)
+subject_train <- read.table("subject_train.txt")
+trainingDataSet <- cbind(subject_train,Y_train,X_train)
+names(trainingDataSet)[1] <- "Subject"
+names(trainingDataSet)[2] <- "Activity"
 
-# Merge the test dataset with the activity number 
+# Merge the test dataset with the activity and subject number 
 Y_test <- read.table("Y_test.txt")
-testDataSet <- cbind(Y_test,X_test)
+subject_test <- read.table("subject_test.txt")
+testDataSet <- cbind(subject_test,Y_test,X_test)
+names(testDataSet)[1] <- "Subject"
+names(testDataSet)[2] <- "Activity"
 
 # Merge the training and test datasets
 mergedData <- rbind(trainingDataSet,testDataSet)
@@ -25,6 +34,11 @@ mergedData <- rbind(trainingDataSet,testDataSet)
 # Add descriptive activity names
 activities <- read.table("activity_labels.txt")
 
-mergedData["V1"]=activities[match(as.character(unlist(mergedData[1])), 
+mergedData["Activity"]=activities[match(as.character(unlist(mergedData[2])), 
                        as.character(activities[[1]])), 2]
-write.table(mergedData,file="mergedData.csv",row.name=FALSE) 
+
+# Extracts only the measurements related to the mean or standard deviation
+requiredColumns <- grep ("mean|std|Subject|Activity|TypeOfData",names(mergedData))
+cleanedData <- mergedData[,requiredColumns]
+
+
